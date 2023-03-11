9a38ba4ff22833b8fdc58f811465f92148f55ea0
Jenya Golovnov <93ave.blues@gmail.com>
2023-03-11 18:00:10 +0100

    exposed weather forecast into console

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const request = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=2bc200a82ad20d864dccf7e7a868edad&query=Berlin';
+const url = 'http://api.weatherstack.com/current?access_key=2bc200a82ad20d864dccf7e7a868edad&query=Berlin&units=f';
 
-request({ url }, (error, response) => {
-    const weatherData = JSON.parse(response.body);
-    console.log(weatherData.current);
+request({ url, json: true }, (error, { body }) => {
+    const weatherData = body.current;
+    const result = `${weatherData.weather_descriptions[0]}. The current temperature is ${weatherData?.temperature} degrees out. It feels like ${weatherData?.feelslike} degrees`;
+    console.log(result);
 });
