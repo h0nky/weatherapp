6b85a851eb389abb538b97475e50bef2a7ebfa75
Jenya Golovnov <93ave.blues@gmail.com>
2023-04-17 22:12:40 +0200

    getGeocode / getWeatherForecast methods / refactoring

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-const request = require('postman-request');
+const { getGeocode, getGeocodeUrl, getWeatherForecast, getWeatherUrl } = require('./utils/geocode');
 
-const url = 'http://api.weatherstack.com/current?access_key=2bc200a82ad20d864dccf7e7a868edad&query=Berlin&units=f';
-const mapBoxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiemgzIiwiYSI6ImNsZzk3eW55ajE0d2QzZ285Z29hcXMzMnIifQ.nRH6H8AQtNM9FkRxitDpsQ&limit=1';
-
-request({ url, json: true }, (error, response) => {
-    if (error) {
-        console.error('Unable to retrieve the forecast!');
-    } else if (response?.body?.error) {
-        console.log('Something wrong with your request!');
-    } else {
-        const weatherData = response.body.current;
-        const result = `${weatherData.weather_descriptions[0]}. The current temperature is ${weatherData?.temperature} degrees out. It feels like ${weatherData?.feelslike} degrees`;
-        console.log(result);
-    }
+getGeocode(getGeocodeUrl('Berlin'), (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
 });
 
-request({ url: mapBoxUrl, json: true }, (error, result) => {
-    if (error) {
-        console.log('Unable to reach the service :(');
-    } else if (!result.body.features.length) {
-        console.log('Something wrong with your request!');
-    } else {
-        const [ long, lang ] = result.body.features[0].center;
-        console.log('Longtitude', long);
-        console.log('Langtitude', lang);
-    }
-});
+getWeatherForecast(getWeatherUrl('Berlin'), (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
+})
---
+++ utils/geocode.js
@@ -0,0 +1,38 @@
+const request = require('postman-request');
+
+const getGeocodeUrl = address => `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiemgzIiwiYSI6ImNsZzk3eW55ajE0d2QzZ285Z29hcXMzMnIifQ.nRH6H8AQtNM9FkRxitDpsQ&limit=1`;
+const getWeatherUrl = location =>  `http://api.weatherstack.com/current?access_key=2bc200a82ad20d864dccf7e7a868edad&query=${location}&units=f`;
+
+const getGeocode = (url, callback) => {
+    request({ url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to location services!', null);
+        } else if (!response.body.features.length) {
+            callback('Something wrong with your request!', null);
+        } else {
+            const [ long, lang ] = response.body.features[0].center;
+            callback(null, {
+                latitude: lang,
+                longtitude: long,
+                location: response.body.features[0].place_name
+            });
+        }
+    })
+}
+
+const getWeatherForecast = (url, callback) => {
+    request({ url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to retrieve the forecast!', null);
+        } else if (response?.body?.error) {
+            callback('Something wrong with your request!', null);
+        } else {
+            const weatherData = response.body.current;
+            const result = `${weatherData.weather_descriptions[0]}. The current temperature is ${weatherData?.temperature} degrees out. It feels like ${weatherData?.feelslike} degrees`;
+            callback(null, result);
+        }
+    });
+}
+
+
+module.exports = { getGeocode, getGeocodeUrl, getWeatherForecast, getWeatherUrl };
